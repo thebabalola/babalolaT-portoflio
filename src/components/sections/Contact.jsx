7653f7ea4a1b8d432c81e24653ae4a53190653f8
thebabalola <t.babalolajoseph@gmail.com>
2026-06-12 04:15:47 +0100

    content: update linkedin url

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -212,7 +212,7 @@ const Contact = () => {
                 <Github className="w-6 h-6" />
               </a>
               <a
-                href="https://linkedin.com/in/babalola-taiwo"
+                href="https://www.linkedin.com/in/babalola-t"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-light-grey hover:text-primary-green transition-all hover:scale-110"
